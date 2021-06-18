f7a6c258deabb081cd9de7c4c210f40f8aded2df
jjtorres93 <it.jj.torres@gmail.com>
2021-06-18 12:24:21 +0100

    ref#0001

---
+++ database/migrations/2021_06_16_120908_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id('id')->nullable(false);
+            $table->string('status')->default("new")->nullable(false);
+            $table->integer('assigned')->nullable(true);
+            $table->string('title')->nullable(true);
+            $table->text('description')->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
